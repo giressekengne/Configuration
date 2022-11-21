feca8285c4d8e49d7eba4caeb10b76fe440d4258
Giresse Kengne <gigatore97@gmail.com>
2022-11-21 10:22:55 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Configuration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            Settings settings = config.GetRequiredSection("settings").Get<Settings>();
+            Console.WriteLine(settings.GIGI);
+            Console.WriteLine(settings.MATHI);
+            
+        }
+      
+        public class Settings
+        {
+            public string MATHI { get; set; }
+            public string GIGI { get; set; }
+        }
+    }
+}
